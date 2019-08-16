67774c76f8ff0ac67e2e35fdd40e16602d8b4733
Gabriel Gradinar <gabrielgradinar@hotmail.com>
2019-08-16 15:38:28 -0300

    ultimos ajustes

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 if(process.env.NODE_ENV == "production"){
     module.exports = {
-        mongoURI: "mongodb+srv://root:root@blogapp-azzxv.mongodb.net/test?retryWrites=true&w=majority"
+        mongoURI: "mongodb+srv://root:root@cluster0-swyzt.mongodb.net/omnistack8?retryWrites=true&w=majority"
     }
 }else{
     module.exports = {
